248bec99b6d3500937b97ff47437cd77b8352a29
IrinaLogvinovich <ruirchik@mail.ru>
2019-02-07 20:24:23 +0500

    Шаблонизация проекта

---
+++ templates/index.php
@@ -0,0 +1,40 @@
+<h2 class="content__main-heading">Список задач</h2>
+
+<form class="search-form" action="index.php" method="post">
+    <input class="search-form__input" type="text" name="" value="" placeholder="Поиск по задачам">
+
+    <input class="search-form__submit" type="submit" name="" value="Искать">
+</form>
+
+<div class="tasks-controls">
+    <nav class="tasks-switch">
+        <a href="/" class="tasks-switch__item tasks-switch__item--active">Все задачи</a>
+        <a href="/" class="tasks-switch__item">Повестка дня</a>
+        <a href="/" class="tasks-switch__item">Завтра</a>
+        <a href="/" class="tasks-switch__item">Просроченные</a>
+    </nav>
+
+    <label class="checkbox">
+        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php if ($data['show_complete_tasks'] === 1):?> <?= "checked";?><?php endif;?>>
+        <span class="checkbox__text">Показывать выполненные</span>
+    </label>
+</div>
+
+<table class="tasks">
+
+<?php foreach ($data['tasks'] as $key => $value):?>
+    <?php if ($data['show_complete_tasks'] === 1 or ($data['show_complete_tasks'] === 0 and $value["is_done"] !== "Да")):?>
+        <tr class="tasks__item task <?php if($value["is_done"] === "Да"):?><?="task--completed";?><?php endif;?>">
+        <td class="task__select">
+            <label class="checkbox task__checkbox">
+            <input class="checkbox__input visually-hidden" type="checkbox" <?php if($value["is_done"] === "Да") : ?><?= "checked"; ?><?php endif; ?>>
+                <span class="checkbox__text"><?=htmlspecialchars($value["name"]);?></span>
+            </label>
+        </td>
+        <td class="task__date"><?=$value["execution_date"];?></td>
+        <td class="task__controls"></td>
+    </tr>
+    <?php endif; ?>
+<?php endforeach; ?>
+
+</table>
